4496e51677d69ae8adf925ec3640600bba850dee
TeacherG104 <bgallagher@509j.net>
2025-12-15 16:00:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 import io, math
